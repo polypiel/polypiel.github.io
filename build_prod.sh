738c20f3a4f7ce56e4b067d48f6bab391e0e117c
Angel Calvo <polloverdetm@gmail.com>
2014-03-27 00:31:11 +0100

    Build scripts updated

--- build_prod.sh
+++ build_prod.sh
@@ -12,6 +12,6 @@ jekyll build
 yui-compressor _build/js/angel.js -o _build/js/angel.js
 yui-compressor _build/js/simplespi.js -o _build/js/simplespi.js
 # Minifies CSS files
-yui-compressor _build/css -o build/animate-custom.css
-yui-compressor _build/lightbox.css -o build/lightbox.css
-yui-compressor -build/normalize.css -o build/normalize.css
+yui-compressor _build/css/animate-custom.css -o _build/css/animate-custom.css
+yui-compressor _build/css/lightbox.css -o _build/css/lightbox.css
+yui-compressor _build/css/normalize.css -o _build/css/normalize.css
