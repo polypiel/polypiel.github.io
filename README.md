eeecb9ec4ce207875dab51ff9fc1968968876a6f
Angel Calvo <hi@angeluiscalvo.com>
2020-08-10 15:59:35 +0200

    Remove sections and SPI navigation

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ My personal web at angelcalvo.com
  * Jekyll
  * Sass
  * [Normalize CSS](https://necolas.github.io/normalize.css/)
- * [Handlebars](https://handlebarsjs.com/)
- * [Magnific popup](https://dimsemenov.com/plugins/magnific-popup/)
- * [Salvattore](https://salvattore.js.org/)
- * Custom SPI navigation
  * Yui-compressor for assets minification
 
 ## Building
--- build.sh
+++ build.sh
@@ -25,13 +25,7 @@ jekyll build
 
 if [ ! -z  "$SASS_FGS" ]
 then
-	# Minifies JS files
-	yui-compressor _build/js/angel.js -o _build/js/angel.js
-	yui-compressor _build/js/simplespi.js -o _build/js/simplespi.js
-	yui-compressor _build/js/handlebars-v2.0.0.js -o _build/js/handlebars-v2.0.0.js
 	# Minifies CSS files
-	yui-compressor _build/css/animate-custom.css -o _build/css/animate-custom.css
-	yui-compressor _build/css/magnific-popup.css -o _build/css/magnific-popup.css
 	yui-compressor _build/css/normalize.css -o _build/css/normalize.css
 	yui-compressor _build/css/angel.css -o _build/css/angel.css
 fi
